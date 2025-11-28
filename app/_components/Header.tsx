6e66752d19f50ca8419499f5fea5b1cb8a21ecca
Kalyan Manna <kalyanmanna439@gmail.com>
2025-11-29 00:22:56 +0530

    Add new shared UI components

---
+++ app/_components/Header.tsx
@@ -0,0 +1,125 @@
+"use client"
+
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "@/components/ui/navigation-menu"
+import Image from 'next/image'
+import { Button } from '@/components/ui/button'
+import Link from "next/link"
+import { UserButton, useUser } from "@clerk/nextjs";
+
+const courses = [
+  {
+    id: 1,
+    name: "HTML",
+    desc: "Learn the fundamentals of HTML and build the structure of modern web pages.",
+    path: "/course/1/detail",
+  },
+  {
+    id: 2,
+    name: "CSS",
+    desc: "Master CSS to style and design responsive, visually appealing web layouts.",
+    path: "/course/2/detail",
+  },
+  {
+    id: 3,
+    name: "React",
+    desc: "Build dynamic and interactive web applications using the React JavaScript library.",
+    path: "/course/3/detail",
+  },
+  {
+    id: 4,
+    name: "React Advanced",
+    desc: "Deep dive into advanced React concepts including hooks, state management, performance optimization, and architectural patterns.",
+    path: "/course/4/detail",
+  },
+  {
+    id: 5,
+    name: "Python",
+    desc: "Learn Python programming from basics to intermediate level, covering logic building, functions, and real-world applications.",
+    path: "/course/5/detail",
+  },
+  {
+    id: 6,
+    name: "Python Advanced",
+    desc: "Master advanced Python concepts such as OOP, modules, APIs, data processing, and automation.",
+    path: "/course/6/detail",
+  },
+  {
+    id: 7,
+    name: "Generative AI",
+    desc: "Explore prompt engineering, LLMs, embeddings, image generation, and build GenAI-powered applications.",
+    path: "/course/7/detail",
+  }
+];
+
+
+const Header = () => {
+ const {user} = useUser();
+
+  return (
+    <div className='fixed z-99 max-w-7xl p-4 flex justify-between items-center w-full'>
+      <div className='flex gap-2 items-center'>
+
+        <Image src="/logo.png" alt="Logo" width={40} height={40} />
+        <h1 className="font-bold text-3xl font-game">CodeBox</h1>
+      </div>
+
+      {/* Navigation Section */}
+      <NavigationMenu>
+        <NavigationMenuList className="gap-8">
+          <NavigationMenuItem>
+            <NavigationMenuTrigger>Courses</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <ul className="grid md:grid-cols-2 gap-2 sm:w-[400px] md:w-[500px] lg:w-{600px]">
+                {courses.map((course, index) => (
+                  <div key={index} className="p-4 hover:bg-accent rounded-xl cursor-pointer">
+                    <h2 className="font-medium">{course.name}</h2>
+                    <p className="text-sm text-gray-500">{course.desc}</p>
+                  </div>
+                ))}
+              </ul>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+
+          <NavigationMenuItem>
+            <NavigationMenuLink>
+              <Link href={"/projects"}>Projects</Link>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+
+          <NavigationMenuItem>
+            <NavigationMenuLink>
+              <Link href={"/pricing"}>Pricing</Link>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+
+          <NavigationMenuItem>
+            <NavigationMenuLink>
+              <Link href={"/contact"}>Contact Us</Link>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+
+        </NavigationMenuList>
+      </NavigationMenu>
+
+      {/* Login Button  */}
+      {!user ?
+
+<Link href={'/sign-in'}>
+      <Button className="font-game text-2xl" variant="pixel">Sign In</Button>
+      </Link>
+      : <div className="flex gap-4 items-center">
+        <Button className="font-game text-2xl" variant="pixel">Dashboard</Button>
+        <UserButton/>
+      </div>}
+    </div>
+  )
+}
+
+export default Header
---
+++ app/_components/Hero.tsx
@@ -0,0 +1,28 @@
+"use client"
+import Link from "next/link";
+
+import Image from 'next/image'
+import { Button } from '@/components/ui/button'
+
+const Hero = () => {
+  return (
+    <div className='w-full relative h-screen overflow-hidden'>
+      <Image src={"/hero.gif"} alt='hero' width={1000} height={1000}
+      className='absolute w-full h-full inset-0 object-cover'/>
+
+      <div className='absolute w-full flex flex-col items-center mt-44'>
+        <h1 className='font-bold text-7xl font-game'>Start Your</h1>
+        <h1 className='font-bold text-8xl font-game text-yellow-400'
+        style={{textShadow: "2px 2px 0 #000, -2px -2px 0 #000, -2px 2px 0 #000,2px -2px 0 #000 "}}
+        >Coding Journey</h1>
+
+        <h2 className='mt-5 font-game text-3xl'>Beginner friendly coding courses & projects</h2>
+        
+        <Link href={'/sign-in'}>
+        <Button variant="pixel" className='mt-7 text-3xl font-game p-6'>GET STARTED</Button></Link>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
