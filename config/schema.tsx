f5366333e23872ea3ab927736826bd9cfd7d3fb3
Kalyan Manna <kalyanmanna439@gmail.com>
2025-12-05 00:14:57 +0530

    Add project configuration utilities

---
+++ config/schema.tsx
@@ -0,0 +1,30 @@
+import { integer, json, pgTable, varchar } from "drizzle-orm/pg-core";
+
+export const usersTable = pgTable("users", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  name: varchar({ length: 255 }).notNull(),
+  //   age: integer().notNull(),
+  email: varchar({ length: 255 }).notNull().unique(),
+  points: integer().default(0),
+  subscription: varchar()
+});
+
+export const courseTable = pgTable("courses", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  courseId: integer().notNull().unique(),
+  title: varchar({ length: 255 }).notNull(),
+  description: varchar({ length: 500 }).notNull(),
+  bannerImage: varchar().notNull(),
+  level: varchar({ length: 50 }).default('Beginner'),
+  tags: varchar({ length: 255 })
+
+});
+
+export const courseChaptersTable = pgTable("courseChapters", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  courseId: integer().notNull(),
+  name: varchar({ length: 255 }).notNull(),
+  description: varchar({ length: 500 }).notNull(),
+  exersices: json(),
+  chapterId: integer().notNull()
+})
