9c5d6c6725e9bb8b24e330c4427f28ac2eccb434
Kalyan Manna <kalyanmanna439@gmail.com>
2025-12-08 00:01:37 +0530

    UI Update: Polished UpgradeToPro component design

--- app/(routes)/dashboard/_components/UpgradeToPro.tsx
+++ app/(routes)/dashboard/_components/UpgradeToPro.tsx
@@ -5,7 +5,7 @@ import Image from "next/image"
 
 const UpgradeToPro = () => {
     return (
-        <div className="flex flex-col items-center px-4 py-5 border-4 rounded-2xl mt-8">
+        <div className="flex flex-col items-center px-4 py-5 border-4 rounded-2xl mt-8 w-full">
             <Image src={'/logo.png'} alt="logo" width={70} height={70} />
             <h2 className="font-game text-3xl">Upgrade To Pro</h2>
             <p className="font-game text-gray-500 text-xl">Join Pro Membership & Get All Course Access</p>
